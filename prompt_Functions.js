23d5fec52e4c14f40fc6e653d6bc5a421fd407af
Volodimir <volodimirzukivskij@gmail.com>
2023-04-24 20:13:49 +0300

    add working program

---
+++ prompt_Functions.js
@@ -0,0 +1,19 @@
+
+import { needRefined,askStudent,good } from "./funtions.js";
+
+const nameFunc = prompt("Введіть функцію");
+
+const changeFunc = (nameFunc) => {
+  switch (nameFunc) {
+    case "need":
+      return needRefined();
+    case "ask":
+      return askStudent();
+case 'good':
+  return good();
+    default:
+      break;
+  }
+};
+
+console.log(changeFunc(nameFunc))   ;
